a1d813a48bf5babd4553280c72dee507aa87fb1e
Ziaul Kamal <codepad@usa.com>
2021-09-08 21:41:55 +0700

    Update loop.ps1
    update

--- loop.ps1
+++ loop.ps1
@@ -1,8 +1,11 @@
-@echo off
-echo RDP CREATION SUCCESSFULL!
-tasklist | find /i "ngrok.exe" >Nul && goto check || echo "Unable to get NGROK tunnel, make sure NGROK_AUTH_TOKEN is correct in Settings > Secrets > Repository secret. Maybe your previous VM is still running: https://dashboard.ngrok.com/status/tunnels" & ping 127.0.0.1 >Nul & exit
-:check
-ping 127.0.0.1 > null
-cls
-echo RDP CREATION SUCCESSFULL!
-goto check
+$i = 360
+
+do {
+
+    Write-Host $i
+
+    Sleep 60
+
+    $i--
+
+} while ($i -gt 0)
